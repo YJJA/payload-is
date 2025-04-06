60de0d4518654a6ea433dac9b0efdf984542cb1a
Hong Liu <hongdoit@icloud.com>
2025-04-07 00:44:33 +0800

    chore: remove jest framework and delete config file and update deps

--- global.d.ts
+++
@@ -1,9 +0,0 @@
-import type { TestingLibraryMatchers } from "@types/testing-library__jest-dom/matchers";
-
-declare module "@jest/expect" {
-  export interface Matchers<R = void, T = {}>
-    extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
-}
-
-import type { JestExpect } from "@jest/expect";
-export declare const expect: JestExpect;
